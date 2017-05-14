a57f21da3de5f06bad382e8baf756b4953964a9c
Chalapathi Raju <chalapathi.raju@imaginnovate.com>
2017-05-14 13:41:52 +0530

    Toggle wishlist icon class without waiting for server response

--- app/overrides/add_wish_list_icon.rb
+++ app/overrides/add_wish_list_icon.rb
@@ -4,3 +4,10 @@ Deface::Override.new(
     insert_before: ".panel .panel-body",
     partial: "spree/ajax_wishlist/wishlist_icon",
 )
+
+Deface::Override.new(
+    virtual_path: "spree/shared/_products",
+    name: "add quick_response_js",
+    insert_after: "[data-hook='products_search_results_heading']",
+    partial: "spree/ajax_wishlist/quick_response",
+)
